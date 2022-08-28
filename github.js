026d05019052f5b445e9a2e9b99005edf5f4f089
Momin <aamirmomin152@gmail.com>
2022-08-28 13:51:54 -0500

    Built html for ui

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = '888aed9310c9d2cf45a1'; //Hide in secrets manager
     this.client_secret = '123d14ea2256f2adc0d40210deef897563d7bfca'; //Hide in secrets manager
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -9,10 +11,17 @@ class Github {
     ?client_id=${this.client_id}
     &client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos
+    ?per_page=${this.repos_count}&sort=${this.repos_sort}
+    ?client_id=${this.client_id}
+    &client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 
     return {
-      profile: profile
+      profile: profile,
+      repos: repos
     }
   }
 }
