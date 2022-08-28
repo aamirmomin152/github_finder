6ccb57a66e96379ed9491dc8600ebf470a8a1889
Momin <aamirmomin152@gmail.com>
2022-08-28 13:55:15 -0500

    Finished building out UI

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
+// Init Github
 const github = new Github;
+// Init UI
 const ui = new UI;
 
 // Search input
 const searchUser = document.getElementById('searchUser');
 
-//Search input event listener
+// Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-  //Get input text
+  // Get input text
   const userText = e.target.value;
 
-  if (userText !== '') {
-    github.getUser(userText)
-      .then(data => {
-        if (data.profile.message === 'Not Found') {
-          //Show alert
-          ui.showAlert('User not found', 'alert alert-danger')
-        }
-        else {
-          //Show Profile
-          ui.showProfile(data.profile);
-          ui.showRepos(data.repos);
-        }
-      })
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    })
   } else {
-    //Clear Profile
+    // Clear profile
     ui.clearProfile();
   }
-});
+}); 
